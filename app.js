04d3c94db061e8e70017e51b967028dec5692c20
Android <shafiqshahzaib@gmail.com>
2023-07-07 12:02:47 +0500

    Todolist Completed

--- app.js
+++ app.js
@@ -2,52 +2,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
+var items=[]
 app.set("view engine", "ejs");
-
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"))
 app.get("/", function(req, res) {
   var today = new Date();
-  var currentDay = today.getDay();
+  var options={
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+  }
 
-var day = "";
-
-switch (currentDay) {
-  case 0:
-    day="Sunday";
-        break;
-        
-        case 1:
-    day="Monday";
-        break;
-        
-        case 2:
-    day="Tueday";
-        break;
-        
-        case 3:
-    day="Wednesday";
-        break;
-        
-        case 4:
-    day="Thursday";
-        break;
-        
-        case 5:
-    day="Friday";
-        break;
-        
-        case 6:
-    day="Saturday";
-        break;
-        
-        default:
-    break;
-}
-
-  
-
-  res.render("list", { KindofDay: day });
+  var day = today.toLocaleDateString("en-US",options)
+  res.render("list", { KindofDay: day,newListItem:items});
 });
+
+app.post("/",function(req,res){
+  var item=req.body.newItem
+  res.redirect("/")
+  items.push(item)
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
